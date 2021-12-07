f36cefefdf748711c9697aa7f61a9332b978d054
lespuch-v <36127590+lespuch-v@users.noreply.github.com>
2021-12-07 17:06:23 +0100

    one

---
+++ scripts.js
@@ -0,0 +1,25 @@
+const secondHand = document.querySelector(".second-hand")
+const minuteHand = document.querySelector(".min-hand")
+const hourHand = document.querySelector(".hour-hand")
+
+function setDate(){
+    const now = new Date()
+
+    const seconds = now.getSeconds()
+    const secondDegrees = ((seconds / 60) * 360) + 90
+    
+    secondHand.style.transform = `rotate(${secondDegrees}deg)`
+    
+    const minute = now.getMinutes()
+    const minutesDegree = ((minute / 60) * 360) + ((seconds/60)*6) + 90;
+    minuteHand.style.transform = `rotate(${minutesDegree}deg)`
+    
+    const hour = now.getHours()
+    const hourDegrees = ((hour / 12) * 360) + ((minute/60)*30) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees})`
+}
+setInterval(setDate,1000)
+
+
+
+setDate()
